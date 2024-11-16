4fc72491066e780aa28567b08402afe05567f2c0
Gladdonillini <tianyi35@illinois.edu>
2024-11-16 12:23:50 -0600

    config

---
+++ config.py
@@ -0,0 +1,4 @@
+# Time configuration
+START_HOUR = 6  # 6 AM
+END_HOUR = 22   # 6 PM
+TIME_INTERVAL = "1H"  # 1 hour intervals
--- static/script.js
+++ static/script.js
@@ -1,19 +1,30 @@
-const times = ['12:00AM', '1:00AM', '2:00AM', '3:00AM', '4:00AM', '5:00AM', '6:00AM',
-    '7:00AM', '8:00AM', '9:00AM', '10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM',
-    '3:00PM', '4:00PM', '5:00PM', '6:00PM', '7:00PM', '8:00PM', '9:00PM',
-    '10:00PM', '11:00PM']
+fetch('/config')
+    .then(response => response.json())
+    .then(config => {
+        const START_HOUR = config.START_HOUR;
+        const END_HOUR = config.END_HOUR;
 
-const selectstarttime = document.getElementById("selectstarttime");
-const selectendtime = document.getElementById("selectendtime");
+        // Generate times array based on configured hours
+        const times = [];
+        for (let hour = START_HOUR; hour <= END_HOUR; hour++) {
+            const timeStr = hour < 12 ? 
+                `${hour}:00AM` : 
+                `${hour === 12 ? 12 : hour-12}:00PM`;
+            times.push(timeStr);
+        }
 
+        const selectstarttime = document.getElementById("selectstarttime");
+        const selectendtime = document.getElementById("selectendtime");
 
-times.forEach(time => {
-    const option = document.createElement("option");
-    option.text = time;
-    selectendtime.appendChild(option);
-});
-times.forEach(time => {
-    const option = document.createElement("option");
-    option.text = time;
-    selectstarttime.appendChild(option);
-});
+        times.forEach(time => {
+            const option = document.createElement("option");
+            option.text = time;
+            selectendtime.appendChild(option);
+        });
+
+        times.forEach(time => {
+            const option = document.createElement("option");
+            option.text = time;
+            selectstarttime.appendChild(option);
+        });
+    });
